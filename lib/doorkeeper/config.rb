bcf1ad787e901f2655823f455d37c5e23ce0f147
Rudth-Mael Galite <rudth-mael@ifeelgoods.com>
2014-04-15 15:14:19 +0200

    Add xframe options for oauth/authorize
    Conflicts:
    	lib/doorkeeper/config.rb

--- lib/doorkeeper/config.rb
+++ lib/doorkeeper/config.rb
@@ -187,6 +187,9 @@ and that your `initialize_models!` method doesn't raise any errors.\n
     option :cors_options,
            :as => :cors_options,
            :default => nil
+    option :xframe_options,
+           :as => :xframe_options,
+           :default => nil
     option :original_resource_owner,
            :as => :original_resource_owner,
            :default => lambda{|routes|
