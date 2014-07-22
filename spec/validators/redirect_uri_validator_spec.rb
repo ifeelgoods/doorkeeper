c66ab70023b5ed5a013b45a781f50c1eec249004
Rudth-Mael Galite <rudth-mael@ifeelgoods.com>
2014-07-22 12:43:05 +0200

    Allow * in domain and expand it with regex Use FuzzyURL to match the given URL Update redirect_uri_validator.rb Added comment to explain why '*' was replaced
    Conflicts:
    	doorkeeper.gemspec

--- spec/validators/redirect_uri_validator_spec.rb
+++ spec/validators/redirect_uri_validator_spec.rb
@@ -75,4 +75,15 @@ describe RedirectUriValidator do
       expect(error).to eq('must be an HTTPS/SSL URI.')
     end
   end
+
+  context 'with wildcard_redirect_uri = true' do
+    before do
+      Doorkeeper.configuration.stub(wildcard_redirect_uri: true)
+    end
+
+    it 'is valid if the domain contains *' do
+      subject.redirect_uri = 'http://*.example.com/callback'
+      expect(subject).to be_valid
+    end
+  end
 end
